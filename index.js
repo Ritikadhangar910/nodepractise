edfcd72959651302f0087920589eb2109f68e67b
dhangarritika <dhangarritika90@gmail.com>
2023-10-31 17:56:01 +0530

    Request / Response

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log("ritika");
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head> <title>home</title> </head>");
+    res.write("<body> <h2> home page </h2> </body>");
+    res.write("</html>");
+  } else if (req.url === "/about") {
+    res.write("<html>");
+    res.write("<head> <title>home</title> </head>");
+    res.write("<body> <h2> about page </h2> </body>");
+    res.write("</html>");
+  } else {
+    res.write("<html>");
+    res.write("<head> <title>home</title> </head>");
+    res.write("<body> <h2> node page </h2> </body>");
+    res.write("</html>");
+  }
+  res.end();
 });
 server.listen(4000);
