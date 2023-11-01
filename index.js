d1c6d3dc0995e0098b81b69160fd9e3273530568
dhangarritika <dhangarritika90@gmail.com>
2023-11-01 09:20:28 +0530

    task1

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.write("<html>");
     res.write("<head> <title>home</title> </head>");
-    res.write("<body> <h2> home page </h2> </body>");
+    res.write(
+      '<body> <form action="/message" method="POST"> <input type="text" name="message"> <button type="summit"> click </button></form> </body>'
+    );
     res.write("</html>");
-  } else if (req.url === "/about") {
-    res.write("<html>");
-    res.write("<head> <title>home</title> </head>");
-    res.write("<body> <h2> about page </h2> </body>");
-    res.write("</html>");
-  } else {
-    res.write("<html>");
-    res.write("<head> <title>home</title> </head>");
-    res.write("<body> <h2> node page </h2> </body>");
-    res.write("</html>");
+    return res.end();
+  } else if (res.url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const paredbody = Buffer.concat(body).toString();
+      const message = paredbody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head> <title>first page </title> </head>");
+  res.write("<body> <h1>hello from node.js </h1> </body>");
+  res.write("</html>");
   res.end();
 });
+
 server.listen(4000);
