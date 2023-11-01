e1d373f791d6dd489d8fa95ceeebf059149a4138
dhangarritika <dhangarritika90@gmail.com>
2023-11-01 20:53:46 +0530

    task 3

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/addproduct", (req, res, next) => {
-  res.send(`<form action="/product" method="POST" >
-  <input type="text" name="title">
-  <input type="text" name="length">
-  <button type="summit" >add product</button> 
-  </form>`);
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello expensee</h1>");
+app.use("/admin", adminRoute);
+app.use("/shop", shopRoute);
+app.use((req, res, next) => {
+  res.status(404).send("<h2>page not found</h2>");
 });
 app.listen(4000);
