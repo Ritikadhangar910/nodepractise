57a83865314c8742158554a7d438b68f16e67dba
dhangarritika <dhangarritika90@gmail.com>
2023-11-01 20:09:48 +0530

    express apis

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/addproduct", (req, res, next) => {
+  res.send(`<form action="/product" method="POST" >
+  <input type="text" name="title">
+  <input type="text" name="length">
+  <button type="summit" >add product</button> 
+  </form>`);
 });
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  res.send(" <h1> hello from express </h1>");
-  next();
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
+app.use("/", (req, res, next) => {
+  res.send("<h1>hello expensee</h1>");
+});
 app.listen(4000);
