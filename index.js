0baabb485def6fb4b7c8481be8947f43025712a8
dhangarritika <dhangarritika90@gmail.com>
2023-11-02 10:44:33 +0530

    task 4

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
-const adminRoute = require("./routes/admin");
-const shopRoute = require("./routes/shop");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/admin", adminRoute);
-app.use("/shop", shopRoute);
-app.use((req, res, next) => {
-  res.status(404).send("<h2>page not found</h2>");
-});
+const RouteLogin = require("./routes/login");
+const Routeshowuser = require("./routes/showuser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use("/", RouteLogin);
+app.use("/", Routeshowuser);
 app.listen(4000);
