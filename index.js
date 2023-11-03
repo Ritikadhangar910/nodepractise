5915a8ea12b06ef3a7cc7311a1d238acf4ec8181
dhangarritika <dhangarritika90@gmail.com>
2023-11-03 10:21:10 +0530

    task 5

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const bodyparser = require("body-parser");
 const app = express();
-const RouteLogin = require("./routes/login");
-const Routeshowuser = require("./routes/showuser");
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use("/", RouteLogin);
-app.use("/", Routeshowuser);
+const path = require("path");
+const bodyParser = require("body-parser");
+const routeAdmin = require("./routes/admin");
+const shopAdmin = require("./routes/shop");
+const routeContact = require("./routes/contact");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", routeAdmin);
+app.use(shopAdmin);
+app.use(routeContact);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404error.html"));
+});
 app.listen(4000);
